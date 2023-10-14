9d8febfcbf79bbff2a4771a6d975c112c607b375
alohrysyhio <alohryeusrahsyhio@gmail.com>
2023-10-14 11:23:08 +0100

    Added git branch support

--- pp.sh
+++ pp.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Function to get the current git branch
+get_git_branch() {
+    local branch=$(git branch --show-current 2>/dev/null)
+    if [[ -n "$branch" ]]; then
+        echo "on $branch"
+    fi
+}
+
 # Function to set the prompt
 set_prompt() {
     local user_host="$(whoami)@$(hostname)"
@@ -7,8 +15,9 @@ set_prompt() {
     local in="┌ in"
     local at="└"
     local colon="\$ "
+    local git_branch=$(get_git_branch)
 
-    PS1="$in $current_dir\n$at $user_host $colon"
+    PS1="$in $current_dir $git_branch\n$at $user_host $colon"
 }
 
 # Call the set_prompt function
