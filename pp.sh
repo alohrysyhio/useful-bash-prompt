0ef0f4b2ef4e5e5c9df09d6a301906d879d87b41
alohrysyhio <alohryeusrahsyhio@gmail.com>
2023-10-14 13:10:17 +0100

    Added clean working tree detection

--- pp.sh
+++ pp.sh
@@ -8,6 +8,17 @@ get_git_branch() {
     fi
 }
 
+# Function to get the current git status
+get_git_status() {
+    if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+        if [[ -z $(git status --porcelain 2>/dev/null) ]]; then
+            echo "✔" # Git repository is clean
+        else
+            echo "✘" # Git repository has uncommitted changes
+        fi
+    fi
+}
+
 # Function to set the prompt
 set_prompt() {
     local user_host="$(whoami)@$(hostname)"
@@ -16,8 +27,9 @@ set_prompt() {
     local at="└"
     local colon="\$ "
     local git_branch=$(get_git_branch)
+    local git_status=$(get_git_status)
 
-    PS1="$in $current_dir $git_branch\n$at $user_host $colon"
+    PS1="$in $current_dir $git_branch $git_status\n$at $user_host $colon"
 }
 
 # Call the set_prompt function
